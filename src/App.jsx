1d2a3d389cd620cc82592346ebf0e09b2f72dfd1
its-imene <justimene04@gmail.com>
2025-12-25 10:45:55 +0100

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -10,7 +10,7 @@ import About from "./components/About";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
 
       <Routes>
@@ -24,18 +24,16 @@ function App() {
               <Stack />
               <Projects />
               <Contact />
-              
             </>
           }
         />
 
         {/* About Page */}
-        <Route path="/#home" element={<Hero />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
